f813fc17b9d41f402f7698fa86324603823cf2b5
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2023-04-22 21:59:09 +0200

    switched to hash routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import PokemonList from "./components/PokemonList";
 import PokemonDetails from "./components/PokemonDetails";
 import Header from "./components/Header";
