7b608bb59f323af5511a347c5a550f8b5701bb51
TheDarkestNightRises <icecold363@gmail.com>
2023-04-23 20:03:27 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
+import React from "react";
+import AppBar from "@mui/material/AppBar";
 
-import Toolbar from '@mui/material/Toolbar';
+import Toolbar from "@mui/material/Toolbar";
 
-import Typography from '@mui/material/Typography';
+import Typography from "@mui/material/Typography";
 
-import IconButton from '@mui/material/IconButton';
+import IconButton from "@mui/material/IconButton";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function Header() {
-  
-    return (
-        <AppBar position="static" style={{ marginBottom: '50px' }}>
-          <Toolbar>
-            <IconButton component={Link} to="/" edge="start"  color="inherit" aria-label="home">
-            <img src="/pikachu.png" alt="Pokemon logo" style={{ width: '25%', height: 'auto' }} />
-            </IconButton>
-            <Typography variant="h3">
-                Pokedex
-            </Typography>
-          </Toolbar>
-        </AppBar>  
-    );
-  }
+  const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png`;
+  return (
+    <AppBar position="static" style={{ marginBottom: "50px" }}>
+      <Toolbar>
+        <IconButton
+          component={Link}
+          to="/"
+          edge="start"
+          color="inherit"
+          aria-label="home"
+        >
+          <img src={imageUrl} alt="Pokemon logo" />
+        </IconButton>
+        <Typography variant="h3">Pokedex</Typography>
+      </Toolbar>
+    </AppBar>
+  );
+}
